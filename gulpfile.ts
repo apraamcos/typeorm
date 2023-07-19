9f961e1f7f93f55a108aff1e9397a3ffd7871093
Rita Rui <rita.rui@qantasloyalty.com>
2023-07-19 17:34:01 +1000

    TEST

--- gulpfile.ts
+++ gulpfile.ts
@@ -148,11 +148,7 @@ export class Gulpfile {
     packagePublishNext() {
         return gulp
             .src("package.json", { read: false })
-            .pipe(
-                shell([
-                    "cd ./build/package && npm publish --tag next --access public",
-                ]),
-            )
+            .pipe(shell(["cd ./build/package && npm publish --tag next"]))
     }
 
     /**
