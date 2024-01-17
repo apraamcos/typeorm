ec8e50843a375e9e32183e4eb71588548f50a5d5
mgao-apra <mgao@apra.com.au>
2024-01-17 12:26:22 +1100

    fix

--- src/driver/snowflake/SnowflakeQueryRunner.ts
+++ src/driver/snowflake/SnowflakeQueryRunner.ts
@@ -4326,4 +4326,16 @@ export class SnowflakeQueryRunner
         )
         return result.length ? true : false
     }
+
+    /**
+     * Change table comment.
+     */
+    changeTableComment(
+        tableOrName: Table | string,
+        comment?: string,
+    ): Promise<void> {
+        throw new TypeORMError(
+            `snowflake driver does not support change table comment.`,
+        )
+    }
 }
