53b3042c0c0ff6917d99c382c546bc0ccdaef3e7
CJ <powerful_j@outlook.com>
2025-02-10 14:44:42 +1100

    fix

--- src/util/Snowflake.ts
+++ src/util/Snowflake.ts
@@ -1,6 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const formatSnowflakeParameter = (arr?: any[]): any[] | undefined => {
     return arr
-        ? arr.map((item) => (typeof item === "undefined" ? null : item))
+        ? arr.map((item) => {
+              if (typeof item === "undefined" || item === null) {
+                  return null
+              }
+              if (item instanceof Date) {
+                  return item
+              }
+              if (typeof item === "object") {
+                  return JSON.stringify(item)
+              }
+              return item
+          })
         : undefined
 }
