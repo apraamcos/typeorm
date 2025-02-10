1b580c502ac18d598b03c2e6f74a1414ea84de14
CJ <powerful_j@outlook.com>
2025-02-10 14:16:30 +1100

    fix

--- src/util/Snowflake.ts
+++ src/util/Snowflake.ts
@@ -1,17 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const formatSnowflakeParameter = (arr: any[]): any[] => {
-    return arr.map((obj) =>
-        obj && Object.entries(obj).length > 0 // Check if obj is non-null and non-empty
-            ? Object.fromEntries(
-                  Object.entries(obj).map(([key, value]) => [
-                      key,
-                      value === undefined
-                          ? null
-                          : typeof value === "object" && value !== null
-                          ? JSON.stringify(value)
-                          : value,
-                  ]),
-              )
-            : obj,
-    )
+export const formatSnowflakeParameter = (arr?: any[]): any[] | undefined => {
+    return arr
+        ? arr.map((item) => (typeof item === "undefined" ? null : item))
+        : undefined
 }
