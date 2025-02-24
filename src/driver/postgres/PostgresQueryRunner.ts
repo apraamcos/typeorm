de7024d1abec4697c3a774682e5fc888bc0504a0
Cyrus Xu <xyy2436@gmail.com>
2025-02-24 16:06:54 +1100

    add error code for reader instance reboot check

--- src/driver/postgres/PostgresQueryRunner.ts
+++ src/driver/postgres/PostgresQueryRunner.ts
@@ -338,6 +338,7 @@ export class PostgresQueryRunner
                 err.code === "ECONNREFUSED" ||
                 err.code === "ECONNRESET" ||
                 err.code === "ETIMEDOUT" ||
+                err.code === "40001" ||
                 err.message === "the database system is in recovery mode" ||
                 err.message === "the database system is starting up" ||
                 (err.message ?? "")
