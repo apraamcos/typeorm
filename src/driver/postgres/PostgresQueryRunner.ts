30b9ca3cd8977c3b6b7a443d314e983cf4599897
Cyrus Xu <xyy2436@gmail.com>
2025-11-12 15:57:25 +1100

    add retry msg

--- src/driver/postgres/PostgresQueryRunner.ts
+++ src/driver/postgres/PostgresQueryRunner.ts
@@ -318,7 +318,13 @@ export class PostgresQueryRunner
                 err.code === "58P01" ||
                 err.message === "the database system is in recovery mode" ||
                 err.message === "the database system is starting up" ||
-                err.message === "query might have conflicted with replica reconnect" ||
+                err.message ===
+                    "query might have conflicted with replica reconnect" ||
+                (err.message ?? "")
+                    .toLowerCase()
+                    .includes(
+                        "query might have conflicted with replica reconnect",
+                    ) ||
                 (err.message ?? "")
                     .toLowerCase()
                     .includes(
